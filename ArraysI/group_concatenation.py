959d327adcc024812c88296b9f5fa09d15532de5
sai_sudha <saisudhavadisina@gmail.com>
2021-06-05 22:55:01 +0530

    Groups all the similar kind of items and arranges lexicographically in alternative manner

--- ArraysI/group_concatenation.py
+++ ArraysI/group_concatenation.py
@@ -1,12 +1,23 @@
 # 9. Consider a string, group the similar characters in combinations. Then, concatenate first element and last element alternatively.
 
-from itertools import groupby
+# 9. Consider a string, group the similar characters in combinations. Then, concatenate first element and last element alternatively.
 
 def rotation(s, n):
-    k = s.casefold
-    tem = ["".join(list(j)) for i, j in groupby(sorted(s))]
-    return tem
+    tem, res = [], ""
+    for i in sorted(set(s.lower())):
+        comb = [i for i in range(n) if s[i].lower() == s]
+        print(comb)
+        p = ""
+        for i in comb:
+            p += s[i]
+        tem.append(p)
+    m = len(tem)
+    for i in range(m // 2):
+        res += tem[i] + tem[-(i + 1)]
+        if m % 2:
+            res += tem[m // 2]
+    return res
 
 if __name__ == "__main__":
     s = input()
-    print(rotation(list(s), len(s)))
+    print(rotation(s, len(s)))
